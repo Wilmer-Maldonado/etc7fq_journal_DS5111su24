a4488f2c639085fb5e4cae7837e8f0f946ad3621
maldonadow2019 <maldonadow2019@gmail.com>
2024-08-04 23:58:36 -0400

    fixed type class notation

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,6 @@
 import string
 import logging
+from typing import List
 
 # add logging
 logging.basicConfig(
@@ -9,7 +10,7 @@ logging.basicConfig(
 
 def clean_text(text: str) -> str:
     # checks input is str
-    assert isinstance(text, (str)), "clean_text input must be a string"
+    assert isinstance(text, str), "clean_text input must be a string"
     # make text all lowercase
     text = text.lower()
     # make translator to remove punctuation from strings
@@ -17,22 +18,22 @@ def clean_text(text: str) -> str:
     # apply translator to text
     cleaned_text = text.translate(translator)
     # checks cleaned_text is still str
-    assert isinstance(cleaned_text, (str))
+    assert isinstance(cleaned_text, str)
     logging.debug("clean_text return is str type")
     # return text with no punctuation
     return cleaned_text
 
 
-def tokenize(text: str) -> list[str]:
+def tokenize(text: str) -> List[str]:
     # checks input is str
-    assert isinstance(text, (str)), "tokenize input must be a string"
+    assert isinstance(text, str), "tokenize input must be a string"
     # remove punctuation from text
     cleaned_text = clean_text(text)
     # makes text into split list of words
     list_words = cleaned_text.split()
     list_words = [str(word) for word in list_words]
     # checks list_words is now list
-    assert isinstance(list_words, (list))
+    assert isinstance(list_words, list)
     logging.debug("tokenize return is list type")
     # returns list of words
     return list_words
@@ -40,7 +41,7 @@ def tokenize(text: str) -> list[str]:
 
 def count_words(text: str) -> dict:
     # checks input is str
-    assert isinstance(text, (str)), "count_words input must be a string"
+    assert isinstance(text, str), "count_words input must be a string"
     # split text into list of words
     all_words = tokenize(text)
     # track word counts in list all_words
@@ -52,7 +53,7 @@ def count_words(text: str) -> dict:
                  \ninto these counts\n\n{all_word_counts}"""
     )
     # checks all_word_counts is now dictionary
-    assert isinstance(all_word_counts, (dict))
+    assert isinstance(all_word_counts, dict)
     logging.debug("count_words return is dict type")
     # return dictionary of word counts
     return all_word_counts
